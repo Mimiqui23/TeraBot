51e9aeb4eb2bebd8ec56662f818724f3ae29d93d
Antoine2lop <38099787+Antoine2lop@users.noreply.github.com>
2018-11-07 13:30:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function play(connection, message) {
   client.on("ready", () => {
   
       console.log("Je suis prêt !");
-      client.user.setActivity("I love lefavrouge :)");
+      client.user.setActivity("revise le reglement mga");
   
   });
   
