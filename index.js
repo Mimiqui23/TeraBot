3cb096ebaf169a48380d39b47e4541697a465ac6
Antoine2lop <38099787+Antoine2lop@users.noreply.github.com>
2018-11-11 19:33:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function play(connection, message) {
   client.on("ready", () => {
   
       console.log("Je suis prêt !");
-      client.user.setActivity('YouTube', { type: 'WATCHING' });
+      client.user.setActivity('MgaYT', { type: 'WATCHING' });
   
   });
   
